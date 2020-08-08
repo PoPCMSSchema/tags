89a177fb904bd7fbaeaa4ba62ba00aa097aa92fd
Leonardo Losoviz <leo@getpop.org>
2020-08-08 16:52:11 +0800

    Include owner in depended migration plugin

--- src/Component.php
+++ src/Component.php
@@ -23,7 +23,7 @@ class Component extends AbstractComponent
     public static function getDependedMigrationPlugins(): array
     {
         return [
-            'migrate-tags',
+            'pop-schema/migrate-tags',
         ];
     }
 

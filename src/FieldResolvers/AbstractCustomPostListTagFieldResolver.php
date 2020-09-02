37c3a3a9fc511f80132d708736eb7509a0ca1bbc
Leonardo Losoviz <leo@getpop.org>
2020-09-02 12:35:19 +0800

    Added typehints and type for getQuery

--- src/FieldResolvers/AbstractCustomPostListTagFieldResolver.php
+++ src/FieldResolvers/AbstractCustomPostListTagFieldResolver.php
@@ -25,8 +25,16 @@ abstract class AbstractCustomPostListTagFieldResolver extends AbstractCustomPost
 
     abstract protected function getQueryProperty(): string;
 
-    protected function getQuery(TypeResolverInterface $typeResolver, $resultItem, string $fieldName, array $fieldArgs = []): array
-    {
+    /**
+     * @param array<string, mixed> $fieldArgs
+     * @return array<string, mixed>
+     */
+    protected function getQuery(
+        TypeResolverInterface $typeResolver,
+        object $resultItem,
+        string $fieldName,
+        array $fieldArgs = []
+    ): array {
         $query = parent::getQuery($typeResolver, $resultItem, $fieldName, $fieldArgs);
 
         $tag = $resultItem;
